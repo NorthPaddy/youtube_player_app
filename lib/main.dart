a032639408887ac81a5a8e2f941d65ba2310d92c
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-01 23:12:43 +0900

    HomePage class(#6)

--- lib/main.dart
+++ lib/main.dart
@@ -19,4 +19,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
